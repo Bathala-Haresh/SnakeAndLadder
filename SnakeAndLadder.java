1e94b0f902df05e1c2536b9a644027e51222e271
Bathala-Haresh <hareshbathala87@gmail.com>
2021-07-03 08:10:31 +0530

    player is starting with position 0

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,8 +1,17 @@
-package snakesAndLaders;
-public class SnakesAndLadder
+/**
+* This program is snake and Ladder game played with single player at start position 0
+*
+* @author  Bathala Haresh
+*/
+
+public class Main
 {
 	public static void main(String[] args) {
-		System.out.println("Welcome to SnakesAndLadder problem");
+		
+		final int player=1;
+		final int position=0;
+		
+		System.out.println("Welcome to snake and ladder problem ");
+		
 	}
 }
-
